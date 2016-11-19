d5a587b545a7dcd81fc5ce454c8dfe321c7dc51d
Stefan Mehnert <StefanMehnert@gmail.com>
2016-11-19 20:37:28 +0100

    Initial leap year implementatioN

---
+++ swift/leap/leap-swift/leap-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  leap-swift
+//
+//  Created by Stefan Mehnert on 19/11/2016.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ swift/leap/leap-swift/leap-swift/Year.swift
@@ -0,0 +1,9 @@
+//
+//  Year.swift
+//  leap-swift
+//
+//  Created by Stefan Mehnert on 19/11/2016.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
